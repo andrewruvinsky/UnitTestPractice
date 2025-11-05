f425450c38a5fa9874d7fb05fd4adf747fba57ee
Andrew Ruvinsky <andrewruvinsky@gmail.com>
2025-11-05 11:39:09 -0800

    add more unit tests

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -2,6 +2,7 @@
  * Unit Tests for Password class
 **/
 
+// IGNORE ERROR: gtest library included in Docker environment, not here
 #include <gtest/gtest.h>
 #include "Password.h"
 
@@ -17,6 +18,48 @@ class PasswordTest : public ::testing::Test
 TEST(PasswordTest, single_letter_password)
 {
 	Password my_password;
-	int actual = my_password.count_leading_characters("Z");
+	int actual = my_password.count_leading_characters("a");
 	ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, two_leading_letters)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("aa");
+	ASSERT_EQ(2, actual);
+}
+
+TEST(PasswordTest, two_leading_followed_by_different_letter)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("aab");
+	ASSERT_EQ(2, actual);
+}
+
+TEST(PasswordTest, case_sensitive)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("aAb");
+	ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, two_subsequent_after_single_different_letter)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("baa");
+	ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, mixed_case_leading_letters)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("AAa");
+	ASSERT_EQ(2, actual);
+}
+
+TEST(PasswordTest, empty_string)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("");
+	ASSERT_EQ(0, actual);
 }
